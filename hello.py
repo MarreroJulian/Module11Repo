24bbe9131225b391d1cefcf997cea8f1205c7f72
MarreroJulian <s3548911@students.fscj.edu>
2025-11-12 21:38:51 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 # Julian Marrero
 # 11/12/2025
 # A simple Python program to print a message
-Print("Hello, World")
 
 # Julian Marrero
 # 11/12/2025
 # added new message
 
 Print("Hello, World!")
+
+Print ("Welcome to Github!")
